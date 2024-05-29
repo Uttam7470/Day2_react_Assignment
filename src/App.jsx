e6860677c6da287ad9672597c4a47952c676844a
Uttam7470 <patidaruttam74@gmail.com>
2024-05-29 23:49:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <div className="main">
+    <h1 className="heading">Learn web development</h1>
+    <div className="content">
+    <p>welcome to the learning area. This set of articels aims to guide complete beginners to web development with all that they need to start coding websites.</p>
+    </div>
+    <div className="content">
+      <p>The aim of this area of MDN is not take you from "beginner" to "expert" but to take from "beginner" to  "comfortable". From there, you should be able to start making our way, learning from  <a href="">the rest of MDN</a>  and other intermediate to advanced resources that assume a lot of previous knowledge</p>
+    </div>
+    <div className="content">
+      <p>if you are complete beginner, web development can be challenging-- we will hold your hand and provide enough details for you to feel comfortable and learn the topics properly. you should feet at home weather you are a student laerning wed development , a teacher looking for class materials, a hobbyyist or someone who just want to understand more about how web technologirs work.</p>
+    </div>
+    </div>
+  )
+}
+
+export default App
